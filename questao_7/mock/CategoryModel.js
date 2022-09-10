91d592985276484ba51af923590a9f89c98fcebe
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-09-10 13:25:36 -0300

    questao 7, alterado recipient para valores inteiros

--- questao_7/mock/CategoryModel.js
+++ questao_7/mock/CategoryModel.js
@@ -9,7 +9,7 @@ exports.modules = function mockCategoryModel(){
     const attendance = faker.helpers.arrayElement(attendances);
     const price = Number(Number(faker.finance.amount()).toFixed(2));
     const totalPrice = Number(Number(faker.finance.amount(100, 10000)).toFixed(2));
-    const recipientAmount = Number(faker.finance.amount(1, 100));
+    const recipientAmount = faker.datatype.number(1, 100);
 
     return new CategoryModel(
         category,
