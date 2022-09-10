4904081c85993429f11741bd39d04689ee2687b6
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-09-10 12:29:07 -0300

    criada função para Mockar o CategoryModel

---
+++ questao_7/mock/CategoryModel.js
@@ -0,0 +1,21 @@
+const { faker } = require('@faker-js/faker');
+const CategoryModel = require('../model/CategoryModel').modules;
+
+exports.modules = function mockCategoryModel(){
+    const categories = ["Alimentação", "Combustível", "Cultura", "Educação", "Flexível", "Transporte", "Saúde"];
+    const attendances = ["Diária", "Mensal", "Anual", "Semanal"];
+    
+    const category = faker.helpers.arrayElement(categories);
+    const attendance = faker.helpers.arrayElement(attendances);
+    const price = Number(Number(faker.finance.amount()).toFixed(2)).toLocaleString(["pt-Br"]);
+    const totalPrice = Number(Number(faker.finance.amount(100, 10000)).toFixed(2)).toLocaleString(["pt-Br"]);
+    const recipientAmount = faker.finance.amount(1, 100);
+
+    return new CategoryModel(
+        category,
+        attendance,
+        price,
+        totalPrice,
+        recipientAmount
+    );
+}
