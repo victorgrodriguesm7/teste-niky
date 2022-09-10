3e712919089629c4ea6f22ce0cdd7cb855bae236
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-09-10 13:22:35 -0300

    Setup da Questão 6, 8 e 10

---
+++ questao_6/src/App.tsx
@@ -0,0 +1,14 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+function App() {
+
+  return (
+    <Container>
+      <Row>
+        Olá mundo
+      </Row>
+    </Container>
+  )
+}
+
+export default App
