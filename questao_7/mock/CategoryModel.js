8293e918a898263fbe90964f9ea92e21ef82a924
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2022-09-10 13:22:57 -0300

    Removida conversão para String na questão 7

--- questao_7/mock/CategoryModel.js
+++ questao_7/mock/CategoryModel.js
@@ -7,9 +7,9 @@ exports.modules = function mockCategoryModel(){
     
     const category = faker.helpers.arrayElement(categories);
     const attendance = faker.helpers.arrayElement(attendances);
-    const price = Number(Number(faker.finance.amount()).toFixed(2)).toLocaleString(["pt-Br"]);
-    const totalPrice = Number(Number(faker.finance.amount(100, 10000)).toFixed(2)).toLocaleString(["pt-Br"]);
-    const recipientAmount = faker.finance.amount(1, 100);
+    const price = Number(Number(faker.finance.amount()).toFixed(2));
+    const totalPrice = Number(Number(faker.finance.amount(100, 10000)).toFixed(2));
+    const recipientAmount = Number(faker.finance.amount(1, 100));
 
     return new CategoryModel(
         category,
